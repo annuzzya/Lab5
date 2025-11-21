0a2165048dcdc1de7c1b62dee82a08d589cccdcd
Anna Kucherova <kycherova07@gmail.com>
2025-11-22 00:51:53 +0200

    Add async tasks for parallel slow and quick computations

--- main.cpp
+++ main.cpp
@@ -16,9 +16,23 @@ void quick(const std::string& name) { compute(name, 1s); }
 
 void work() {
     using clock = std::chrono::steady_clock;
+
     auto t0 = clock::now();
+
+    auto fut_C2 = std::async(std::launch::async, [](){
+        slow("C2");
+        return 0;
+    });
+
+    auto fut_B2B3 = std::async(std::launch::async, [](){
+        quick("B2");
+        quick("B3");
+        return 0;
+    });
+
     auto t1 = clock::now();
     auto elapsed = std::chrono::duration<double>(t1 - t0).count();
+
     std::cout << "Elapsed (s): " << elapsed << '\n';
 }
 
